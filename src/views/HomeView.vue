c4351374724f0b7be55b25cf71eb94cd7ab2858b
DaviAugustoMS     <jeffersonrochadasilva667@gmail.com>
2022-12-26 11:58:02 -0300

    estudando vue com typeScript

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>{{ rename(name) }}</h1>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
+<script lang="ts" setup>
+const name = "jefferson";
+const rename = (item: string): string => {
+  return item.toUpperCase();
+};
 </script>
